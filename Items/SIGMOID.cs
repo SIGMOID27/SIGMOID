8333f11544b16492a55c3e453e07e101864ffd3c
SIGMOID27 <117216536+SIGMOID27@users.noreply.github.com>
2022-11-03 14:57:51 -0500

    swag

--- Items/SIGMOID.cs
+++ Items/SIGMOID.cs
@@ -9,21 +9,21 @@ namespace SIGMOID.Items
 		public override void SetStaticDefaults()
 		{
 			// DisplayName.SetDefault("SIGMOID"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			Tooltip.SetDefault("sigmoid sword");
 		}
 
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 10000;
 			Item.DamageType = DamageClass.Melee;
-			Item.width = 40;
-			Item.height = 40;
-			Item.useTime = 20;
-			Item.useAnimation = 20;
+			Item.width = 1000;
+			Item.height = 1000;
+			Item.useTime = 1;
+			Item.useAnimation = 1;
 			Item.useStyle = 1;
-			Item.knockBack = 6;
-			Item.value = 10000;
-			Item.rare = 2;
+			Item.knockBack = 0;
+			Item.value = 10000000;
+			Item.rare = 5;
 			Item.UseSound = SoundID.Item1;
 			Item.autoReuse = true;
 		}
--- SIGMOID.cs
+++ SIGMOID.cs
@@ -4,5 +4,6 @@ namespace SIGMOID
 {
 	public class SIGMOID : Mod
 	{
+		
 	}
 }
